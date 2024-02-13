c238eec2aca1128cb0eed58988de3c3c7024583a
Gabriele Di Rocco <gabriele.dirocco000@gmail.com>
2024-02-13 13:29:54 +0100

    card movies

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,5 +3,15 @@
     <title>Home</title>
 @endsection
 @section('content')
-    <h1>Hello, World!</h1>
+    <h1>Movies</h1>
+    <ul style="list-style: none;">
+        @foreach($movies as $movie)
+        
+        <li> Titolo: {{$movie -> title}}</li>
+        <li>Titolo Originale: {{$movie -> original_title}}</li>
+        <li>Nazionalità: {{$movie -> nationality}}</li>
+        <li>Voto:{{$movie -> vote}}</li>
+        <br>
+        @endforeach
+    </ul>
 @endsection
